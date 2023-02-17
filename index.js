7e385dfd21e6b40f616e906fcd21ecb8c97915ce
brunovonkoln <brunovonkoln@gmail.com>
2023-02-17 20:16:10 -0300

    ..

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const btnEl = document.getElementById("btn")
 const olavoEL = document.getElementById("olavo")
 const apiURL = "olavo.json"
 
+let frasesUsadas = [];
+
 async function getOlavo()
 {
   try
@@ -13,7 +15,15 @@ async function getOlavo()
     const data = await response.json()        
     btnEl.disabled = false
     btnEl.innerText = "Frase"
-    olavoEL.innerText = data[Math.floor(Math.random() * data.length)].olavo;
+    // olavoEL.innerText = data[Math.floor(Math.random() * data.length)].olavo;
+    do {
+      fraseSelecionada = data[Math.floor(Math.random() * data.length)].olavo;
+    } while (frasesUsadas.includes(fraseSelecionada));
+
+    frasesUsadas.push(fraseSelecionada);
+
+    olavoEL.innerText = fraseSelecionada
+
   } catch (error)
   {
    olavoEL.innerText = "Aconteceu um erro, tente novamente mais tarde!";
@@ -22,5 +32,14 @@ async function getOlavo()
    console.log(error)
   } 
 }
+
 btnEl.addEventListener("click", getOlavo)
 
+
+// olavoEL.addEventListener('click', () => {
+//   if (data.length === apiURL.length) {
+//     // Se todas as frases já foram usadas, reiniciar a lista de frases usadas
+//     data = [];
+//   }
+//   getOlavo();
+// });
